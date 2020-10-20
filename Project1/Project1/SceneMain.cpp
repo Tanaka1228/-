dd55db3d2cfedc9fd0d033b6e5af328b65c1fd17
泰壱　河村 <h1202019034@hiratagakuen.onmicrosoft.com>
2020-10-20 16:39:33 +0900

    SceneMain.cppの初期化メソッドを変更

--- Project1/Project1/SceneMain.cpp
+++ Project1/Project1/SceneMain.cpp
@@ -4,6 +4,7 @@
 
 //GameLで使用するヘッダー
 #include"GameL\DrawTexture.h"
+#include"GameL\SceneObjManager.h"
 
 //使用するネームスペース
 using namespace GameL;
@@ -29,6 +30,10 @@ void CSceneMain::InitScene()
 {
 	//外部グラフィックファイルを読み込み0番に登録(800×600ピクセル)
 	Draw::LoadImage(L"Hero.png", 0, TEX_SIZE_800);
+
+	//主人公オブジェクト作成
+	CObjHero* obj = new CObjHero(); //主人公オブジェクト作成
+	Objs::InsertObj(obj, OBJ_HERO, 1); //作った主人公オブジェクトをオブジェクトマネージャーに登録
 }
 
 //ゲームメイン実行中メソッド
