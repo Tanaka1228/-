399f5dffe69ad55e26910d9da41df92cf33c9f73
泰壱　河村 <h1202019034@hiratagakuen.onmicrosoft.com>
2020-10-20 15:50:36 +0900

    SceneMainのCppのベース

---
+++ Project1/Project1/SceneMain.cpp
@@ -0,0 +1,26 @@
+//STLデバッグ機能をOFFにする
+#define _SECURE_SCR (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+#include"SceneMain.h"
+#include"GameHead.h"
+
+CSceneMain::CSceneMain()
+{
+
+}
+
+CSceneMain::~CSceneMain()
+{
+
+}
+
+void CSceneMain::InitScene()
+{
+
+}
+
+void CSceneMain::Scene()
+{
+
+}
