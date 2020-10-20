7f074a05c525cc3a0c2d40155144836829001b8c
綾斗 冨永 <h1202019023@hiratagakuen.onmicrosoft.com>
2020-10-20 15:25:39 +0900

    タイトル作成

---
+++ Project1/Project1/SceneTitle.cpp
@@ -0,0 +1,25 @@
+//STLデバッグ機能をOFFにする
+#define _SECURE_SCL(0)
+#define _HAS_ITERATOR_DEBUGGING(0)
+
+//GameLで使用するヘッダー
+#include"GameL\SceneObjManager.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//使用ヘッダー
+#include"SceneMain.h"
+#include"GameHead.h"
+
+//コンストラクタ
+CSceneTitle::CSceneTitle()
+{
+
+}
+
+//デストラクタ
+CSceneTitle::~CSceneTitle()
+{
+
+}
