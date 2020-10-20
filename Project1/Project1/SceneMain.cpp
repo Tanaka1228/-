1362800f145e9ccd7429aab932b1dcee246fed60
泰壱　河村 <h1202019034@hiratagakuen.onmicrosoft.com>
2020-10-20 15:52:47 +0900

    SceneMainのコメント記入

--- Project1/Project1/SceneMain.cpp
+++ Project1/Project1/SceneMain.cpp
@@ -2,24 +2,29 @@
 #define _SECURE_SCR (0)
 #define _HAS_ITERATOR_DEBUGGING (0)
 
+//使用ヘッダー
 #include"SceneMain.h"
 #include"GameHead.h"
 
+//コンストラクタ
 CSceneMain::CSceneMain()
 {
 
 }
 
+//デストラクタ
 CSceneMain::~CSceneMain()
 {
 
 }
 
+//ゲームメイン初期化メソッド
 void CSceneMain::InitScene()
 {
 
 }
 
+//ゲームメイン実行中メソッド
 void CSceneMain::Scene()
 {
 
