d6a43d321e3e018d42ea2faeaa31a2d01ad89c66
綾斗 冨永 <h1202019023@hiratagakuen.onmicrosoft.com>
2020-10-21 15:37:34 +0900

    ObjTitle作成

---
+++ Project1/Project1/ObjTitle.cpp
@@ -0,0 +1,28 @@
+//使用するヘッダ―ファイル
+#include"GameL\DrawFont.h"
+#include"GameL\WinInputs.h"
+
+#include"GameHead.h"
+#include"ObjTitle.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjTitle::Init()
+{
+
+}
+
+//アクション
+void CObjTitle::Action()
+{
+
+}
+
+//ドロー
+void CObjTitle::Draw()
+{
+	float c[4] = { 1.0f,1.0f,1.0f,1.0f, };
+	Font::StrDraw(L"ARTIFICIAL HUMAN ～無人世界の旅～", 0, 0, 32, c);
+}
