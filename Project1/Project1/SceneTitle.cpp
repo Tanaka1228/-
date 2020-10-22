55afe764a3f1ded9485e6c99b12b9c17430192e7
綾斗 冨永 <h1202019023@hiratagakuen.onmicrosoft.com>
2020-10-22 16:10:56 +0900

    ゲームタイトルの背景処理

--- Project1/Project1/SceneTitle.cpp
+++ Project1/Project1/SceneTitle.cpp
@@ -5,6 +5,7 @@
 //GameLで使用するヘッダー
 #include"GameL\SceneObjManager.h"
 #include"GameL\DrawFont.h"
+#include"GameL/DrawTexture.h"
 
 //使用するネームスペース
 using namespace GameL;
@@ -34,4 +35,18 @@ void CSceneTitle::InitScene()
 	//タイトルオブジェクト作成
 	CObjTitle* obj = new CObjTitle();	//タイトルオブジェクト作成
 	Objs::InsertObj(obj, OBJ_TITLE, 10);//主人公オブジェクト登録
+										
+	//外部グラフィックファイルを読み込み0番に登録(512×512ピクセル)あまり関係ないらしい
+	Draw::LoadImage(L"Sifi_town3_4n-480x270.png", 4, TEX_SIZE_512);//弾丸グラフィック
+
+	//主人公オブジェクト作成
+	CObjTitleBackground* back = new CObjTitleBackground(); 
+	Objs::InsertObj(back, OBJ_TITLE_BACKGROUND, 4); 
+
+}
+
+//ゲームメイン実行中メソッド
+void CSceneTitle::Scene()
+{
+
 }
