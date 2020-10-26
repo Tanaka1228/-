3af594e1c48e2ad9ad7326169c4ac405a833a3b7
DEV1-21\GA1A <h1202016013@hiratagakuen.onmicrosoft.com>
2020-10-26 13:58:27 +0900

    Stageの作成

--- Project1/Project1/SceneMain.cpp
+++ Project1/Project1/SceneMain.cpp
@@ -49,29 +49,12 @@ void CSceneMain::InitScene()
 	Objs::InsertObj(obj, OBJ_HERO, 1); //作った主人公オブジェクトをオブジェクトマネージャーに登録
 
 	//敵機オブジェクト作成
-	CObjEnemy* obj_enemy = new CObjEnemy(800,300); //敵機オブジェクト作成
+	CObjEnemy* obj_enemy = new CObjEnemy(800, 300); //敵機オブジェクト作成
 	Objs::InsertObj(obj_enemy, OBJ_ENEMY, 1); //作った敵機オブジェクトをオブジェクトマネージャーに登録
 
-	//攻撃する敵機オブジェクト作成
-	CObjAttackEnemy* obj_attack_enemy = new CObjAttackEnemy(700,220); //攻撃する敵機オブジェクト作成
-	Objs::InsertObj(obj_attack_enemy, OBJ_ATTACK_ENEMY, 1); //攻撃する敵機オブジェクトをオブジェクトマネージャーに登録
-
-	//誘導敵機オブジェクト作成
-	CObjHomingEnemy* obj_homing_enemy = new CObjHomingEnemy(700, 50); //誘導敵機オブジェクト作成
-	Objs::InsertObj(obj_homing_enemy, OBJ_HOMING_ENEMY, 1); //誘導敵機オブジェクトをオブジェクトマネージャーに登録
-
-	//Sin敵機オブジェクト作成
-	CObjSinEnemy* obj_sin_enemy = new CObjSinEnemy(700, 300); //Sin敵機オブジェクト作成
-	Objs::InsertObj(obj_sin_enemy, OBJ_SIN_ENEMY, 1); //Sin敵機オブジェクトをオブジェクトマネージャーに登録
-
-	//拡散敵機オブジェクト作成
-	CObjDiffusionEnemy* obj_diffusion_enemy = new CObjDiffusionEnemy(500, 300); //Sin敵機オブジェクト作成
-	Objs::InsertObj(obj_diffusion_enemy, OBJ_DIFFUSION_ENEMY, 1); //Sin敵機オブジェクトをオブジェクトマネージャーに登録
-
-	//誘導弾丸オブジェクト作成
-	CObjHomingBullet* obj_homing_bullet = new CObjHomingBullet(600, 500); //誘導弾オブジェクト作成
-	Objs::InsertObj(obj_homing_bullet,OBJ_HOMING_BULLET, 1); //誘導弾丸オブジェクトをオブジェクトマネージャーに登録
-
+	//タイム初期化
+	m_time = 0;
+	
 	CObjMain* p = new CObjMain();
 	Objs::InsertObj(p, OBJ_MAIN,1);
 
@@ -80,5 +63,36 @@ void CSceneMain::InitScene()
 //ゲームメイン実行中メソッド
 void CSceneMain::Scene()
 {
+	m_time++;
 
+	if (m_time == 100)
+	{
+		//誘導敵機オブジェクト作成
+		CObjHomingEnemy* obj_homing_enemy = new CObjHomingEnemy(700, 50); //誘導敵機オブジェクト作成
+		Objs::InsertObj(obj_homing_enemy, OBJ_HOMING_ENEMY, 1); //誘導敵機オブジェクトをオブジェクトマネージャーに登録
+	}
+	else if (m_time == 200)
+	{
+		//Sin敵機オブジェクト作成
+		CObjSinEnemy* obj_sin_enemy = new CObjSinEnemy(700, 300); //Sin敵機オブジェクト作成
+		Objs::InsertObj(obj_sin_enemy, OBJ_SIN_ENEMY, 1); //Sin敵機オブジェクトをオブジェクトマネージャーに登録
+	}
+	else if (m_time == 300)
+	{
+		//攻撃する敵機オブジェクト作成
+		CObjAttackEnemy* obj_attack_enemy = new CObjAttackEnemy(700, 220); //攻撃する敵機オブジェクト作成
+		Objs::InsertObj(obj_attack_enemy, OBJ_ATTACK_ENEMY, 1); //攻撃する敵機オブジェクトをオブジェクトマネージャーに登録
+	}
+	else if (m_time == 400)
+	{
+		//拡散敵機オブジェクト作成
+		CObjDiffusionEnemy* obj_diffusion_enemy = new CObjDiffusionEnemy(500, 300); //Sin敵機オブジェクト作成
+		Objs::InsertObj(obj_diffusion_enemy, OBJ_DIFFUSION_ENEMY, 1); //Sin敵機オブジェクトをオブジェクトマネージャーに登録
+	}
+	else if (m_time == 500)
+	{
+		//誘導弾丸オブジェクト作成
+		CObjHomingBullet* obj_homing_bullet = new CObjHomingBullet(600, 500); //誘導弾オブジェクト作成
+		Objs::InsertObj(obj_homing_bullet, OBJ_HOMING_BULLET, 1); //誘導弾丸オブジェクトをオブジェクトマネージャーに登録
+	}
 }
