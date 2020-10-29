354c59dbfeca3a6d5d553111537519689265cd4c
泰壱 河村 <h1202019034@hiratagakuen.onmicrosoft.com>
2020-10-29 16:30:30 +0900

    カーソルアップデート

--- Project1/Project1/ObjTitle.cpp
+++ Project1/Project1/ObjTitle.cpp
@@ -19,8 +19,8 @@ void CObjTitle::Init()
 	 m_mou_y = 0.0f;
 	 m_mou_r = false;
 	 m_mou_l = false;
-	 m_y = 300;
-	 m_x = 250;
+	 m_y = 345;
+	 m_x = 240;
 	 m_key_dy = 0.0f;
 	 m_key_dy = 0.0f;
 	 m_key_enter = false;
@@ -45,8 +45,8 @@ void CObjTitle::Action()
 		 m_y += 5.0f;
 	 }
 
-	 //マウスの位置とクリックする場所で当たり判定
-	 if (m_x > 240 && m_x < 500 && m_y>340 && m_y < 370)
+	 //カーソルの位置とクリックする場所で当たり判定
+	 if (m_x > 200 && m_x < 550 && m_y>320 && m_y < 380)
 	 {
 		 //マウスボタンが押されたらメインに還移
 		 if (m_key_enter == true)
@@ -54,7 +54,17 @@ void CObjTitle::Action()
 			 Scene::SetScene(new CSceneMain());
 		 }
 	 }
-	 
+
+	 if (m_x > 200 && m_x < 550 && m_y>440 && m_y < 530)
+	 {
+		 //エンターキーが押されたらメインに還移
+		 if (m_key_enter == true)
+		 {
+			 Scene::SetScene(nullptr);
+		 }
+	 }
+
+
 	//if (m_mou_x > 250 && m_mou_x < 500 && m_mou_y>380 && m_mou_y < 430)
 	//{
 	//	 //マウスボタンが押されたらメインに還移
@@ -73,14 +83,7 @@ void CObjTitle::Action()
 	//	 }
 	// }
 	// 
-	 if (m_x > 240 && m_x < 500 && m_y>480 && m_y < 530)
-	 {
-		 //マウスボタンが押されたらメインに還移
-		 if (m_key_enter == true)
-		 {
-			 Scene::SetScene(nullptr);
-		 }
-	 }
+
 
 
 	////エンターキーを押してシーン：ゲームTitleに移行する
@@ -121,31 +124,30 @@ void CObjTitle::Draw()
 	//Font::StrDraw(L"◆      設定     ", 250, 450, 32, c);
 	//
 	//シャットダウン
+
+
 	Font::StrDraw(L"◆ シャットダウン", 250, 500, 32, c);
 
-
-	//仮マウス位置表示
-	/*wchar_t str[256];
-	swprintf_s(str, L"x=%f,y=%f", m_mou_x, m_mou_y);
-	Font::StrDraw(str, 20, 20, 12, c);*/
-
-
 	//仮矢印位置表示
-	wchar_t str[256];
-	swprintf_s(str, L"X %d  下 = %d", m_x, m_y);
-	Font::StrDraw(str, m_x, m_y, 32, c);// X  Y  大きさ 
+	//wchar_t str[256];
+	//swprintf_s(str, L"X %d  下 = %d", m_x, m_y);
+	//Font::StrDraw(str, m_x, m_y, 32, c);// X  Y  大きさ 
 
+	//切り取り位置の設定
+	src.m_top = 0.0f;   //y
+	src.m_left = 0.0f;  //x
+	src.m_right = 174.0f; //x
+	src.m_bottom = 80.0f; //y
 
+	//表示位置の設定
+	dst.m_top = 0.0f+m_y;//y
+	dst.m_left = 0.0f+m_x;//x
+	dst.m_right = 300.0f+m_x;//x
+	dst.m_bottom =32.0f+m_y; //y
 
-	////仮マウスのボタンの状態
-	//if(m_mou_r == true)
-	//	Font::StrDraw(L"R=押している", 20, 30, 12, c);
-	//else
-	//	Font::StrDraw(L"R=押していない", 20, 30, 12, c);
-	//if(m_mou_l == true)
-	//	Font::StrDraw(L"L=押している", 20, 40, 12, c);
-	//else
-	//	Font::StrDraw(L"L=押していない", 20, 40, 12, c);
+	//0番目に登録したグラフィックをstc・dst・cの情報を元に描画
+	Draw::Draw(5, &src, &dst, c, 0.0f);
+
 	
 	//仮矢印のボタンの状態
 	if (m_key_enter == true)
