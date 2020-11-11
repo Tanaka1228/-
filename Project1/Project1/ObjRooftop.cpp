69bc98203448be6085bcf216048e960a78263854
DEV1-21\GA1A <h1202016013@hiratagakuen.onmicrosoft.com>
2020-11-11 10:27:50 +0900

    ブロックの表示

--- Project1/Project1/ObjRooftop.cpp
+++ Project1/Project1/ObjRooftop.cpp
@@ -18,7 +18,7 @@ void CObjRooftop::Init()
 	//マップ情報
 	int block_data[25][25] =
 	{
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -185,42 +185,38 @@ void CObjRooftop::Draw()
 	src.m_right = 800.0f ;
 	src.m_bottom = 784.0f ;
 	
-	
+	//表示位置の設定
+	dst.m_top = 0.0f + my_scroll;    //y
+	dst.m_left = 0.0f + mx_scroll;   //x
+	dst.m_right = 800.0f + mx_scroll;  //x
+	dst.m_bottom = 600.0f + my_scroll; //y
+
+	/*dst.m_top = -100.0f + my_scroll;
+	dst.m_left = -100.0f + mx_scroll;
+	dst.m_right = 1500.0f + mx_scroll;
+	dst.m_bottom = 1468.0f + my_scroll;*/
+
+	Draw::Draw(10, &src, &dst, c, 0.0f);
 
 	//マップチップによるblock設置
 	for (int i = 0; i < 25; i++)
 	{
 		for (int j = 0; j < 25; j++)
 		{
-			if (m_map[i][j]==0)//床
-			{
-				//表示位置の設定
-				dst.m_top    = 0.0f     + my_scroll;    //y
-				dst.m_left   = 0.0f     + mx_scroll;   //x
-				dst.m_right  = 800.0f   + mx_scroll;  //x
-				dst.m_bottom = 600.0f   + my_scroll; //y
-
-				/*dst.m_top = -100.0f + my_scroll;
-				dst.m_left = -100.0f + mx_scroll;
-				dst.m_right = 1500.0f + mx_scroll;
-				dst.m_bottom = 1468.0f + my_scroll;*/
-
-				Draw::Draw(10, &src, &dst, c, 0.0f);
-	
-			}
+			
 			if (m_map[i][j] == 1)
 			{
 				//切り取り位置の設定
-				src.m_top = 0.0f;   //y
+				src.m_top = 350.0f;   //y
 				src.m_left = 0.0f; //x
-				src.m_right = 150.0f; //x
-				src.m_bottom = 75.0f; //y
+				src.m_right = 75.0f; //x
+				src.m_bottom = 500.0f; //y
 
 				//表示位置の設定
-				dst.m_top =  32.0f;
-				dst.m_left =  32.0;
-				dst.m_right = dst.m_left + 32.0;
-				dst.m_bottom = dst.m_top + 32.0;
+				dst.m_top =  i*32.0f +my_scroll;
+				dst.m_left =  j*32.0 + mx_scroll;
+				dst.m_right = dst.m_left+32.0f + mx_scroll;
+				dst.m_bottom = dst.m_top+32.0f + my_scroll;
 
 				//描画
 				Draw::Draw(13, &src, &dst, c, 0.0f);
