c97855755c4e373b3188e5d7ecb55bfe99aa6db5
h1202019034@hiratagakuen.onmicrosoft.com <h1202019034@hiratagakuen.onmicrosoft.com>
2020-11-19 20:56:27 +0900

    当たり判定追加

--- Project1/Project1/ObjHospital.cpp
+++ Project1/Project1/ObjHospital.cpp
@@ -30,6 +30,172 @@ void CObjHospital::Init()
 //アクション
 void CObjHospital::Action()
 {
+	//主人公の位置を取得
+	CObjHero* hero = (CObjHero*)Objs::GetObj(OBJ_HERO);
+	float hx = hero->GetX2();//スクロール
+	float hy = hero->GetY2();
+
+
+	if (hero->GetRight() == false)
+	{
+		//後方スクロールライン　←
+		if (hx < 400)
+		{
+			hero->SetX2(400); //主人公はラインを超えないようにする
+			mx_scroll -= -6.0f + hero->GetVX(); //主人公は本来動くべき分の値をm_scrollに加える
+		}
+	}
+
+	if (hero->GetLeft() == false)
+	{
+		//前方スクロールライン →
+		if (hx > 400)
+		{
+			hero->SetX2(400); //主人公はラインを超えないようにする
+			mx_scroll -= 6.0f + hero->GetVX(); //主人公は本来動くべき分の値をm_scrollに加える
+
+		}
+	}
+
+	if (hero->GetDown() == false)
+	{
+		//スクロールライン　↑
+		if (hy > 300)
+		{
+			hero->SetY2(300); //主人公はラインを超えないようにする
+			my_scroll -= 6.0f + hero->GetVY(); //主人公は本来動くべき分の値をm_scrollに加える
+
+		}
+	}
+
+	if (hero->GetUp() == false)
+	{
+		//スクロールライン　↓
+		if (hy < 300)
+		{
+			hero->SetY2(300); //主人公はラインを超えないようにする
+			my_scroll -= -6.0f + hero->GetVY(); //主人公は本来動くべき分の値をm_scrollに加える
+		}
+	}
+
+
+
+
+
+	//主人公の衝突状態確認用フラグの初期化
+	hero->SetUp(false);
+	hero->SetDown(false);
+	hero->SetLeft(false);
+	hero->SetRight(false);
+
+
+
+
+
+
+	//m_mapの全要素にアクセス
+	for (int i = 0; i < 25; i++)
+	{
+		for (int j = 0; j < 25; j++)
+		{
+			if (m_map[i][j] > 0 && m_map[i][j] != 15)
+			{
+
+				//要素番号を座標に変更
+				float x = j * 32.0f;
+				float y = i * 32.0f;
+
+				//主人公とブロックの当たり判定
+				if ((hx + (-mx_scroll) + 64.0f > x) && (hx + (-mx_scroll) < x + 64.0f) && (hy + (-my_scroll) + 64.0f > y) && (hy + (-my_scroll) < y + 64.0f))
+				{
+
+					//上下左右判定
+
+					//vectorの作成
+					float vx = (hx + (-mx_scroll)) - x;
+					float vy = (hy + (-my_scroll)) - y;
+
+					//長さを求める
+					float len = sqrt(vx * vx + vy * vy);//sqrt関数は、平方根を返す
+
+					//角度を求める
+					float r = atan2(vy, vx);//atan2関数はアークタンジェントを返す
+					r = r * 180.0f / 3.14f;
+
+					if (r <= 0.0f)//abs関数は、値の絶対値を求める 符号を無視した値
+						r = abs(r);
+					else
+						r = 360.0f - abs(r);
+
+					//lenがある一定の長さのより短い場合判定に入る
+					if (len < 44.0f)
+					{
+
+						//角度で上下左右を判定
+						if ((r < 45 && r >= 0) || r > 315)
+						{
+							//右
+							hero->SetRight(true);//主人公の左の部分が衝突している
+							hero->SetX2(x + 40.0f + (mx_scroll));//ブロックの位置+主人公の幅
+							hero->SetVX(0.0f);//-VX*反発係数
+						}
+						if (r > 45 && r < 135)
+						{
+							//上
+							hero->SetDown(true);//主人公の下の部分が衝突している
+							hero->SetY2(y - 40.0f + (my_scroll));//ブロックの位置-主人公の幅
+							hero->SetVY(0.0f);//-VX*反発係数
+
+						}
+						if (r > 135 && r < 225)
+						{
+							//左
+							hero->SetLeft(true);//主人公の右の部分が衝突している
+							hero->SetX2(x - 40.0f + (mx_scroll));//ブロックの位置-主人公の幅
+							hero->SetVX(0.0f);//-VX*反発係数
+						}
+						if (r > 225 && r < 315)
+						{
+							//下
+							hero->SetUp(true);//主人公の上の部分が衝突している
+							hero->SetY2(y + 40.0f + (my_scroll));//ブロックの位置+主人公の幅
+							hero->SetVY(0.0f);//-VX*反発係数
+						}
+						if (m_map[i][j] == 41 || m_map[i][j] == 42)//ドアに入ると拠点に移動
+						{
+							Scene::SetScene(new CSceneBlock());
+						}
+					}
+				}
+
+
+
+			}
+		}
+	}
+
+	//敵出現ライン
+	//float Xline = hx + (-mx_scroll) + 400;
+	//float Yline = hy + (my_scroll)-100;
+
+	//int ex = ((int)Xline) / 32;
+	//int ey = ((int)Yline) / 32;
+
+	//for (int i = 0; i < 25; i++)
+	//{
+	//	for (int j = 0; j < 25; j++)
+
+	//		if (m_map[i][ex] == 15)
+	//		{
+	//			//誘導敵機オブジェクト作成
+	//			CObjRooftopBoss* obj_rooftop_boss = new CObjRooftopBoss(ex * 32, i * 32); //誘導敵機オブジェクト作成
+	//			Objs::InsertObj(obj_rooftop_boss, OBJ_ROOF_TOP_BOSS, 4); //誘導敵機オブジェクトをオブジェクトマネージャーに登録
+
+	//			m_map[i][ex] = 0;
+	//		}
+
+
+	//}
 
 }
 //ドロー
@@ -41,15 +207,15 @@ void CObjHospital::Draw()
 	RECT_F dst;//描画先表示位置
 
 	//背景表示
-	src.m_top = 0.0f;
-	src.m_left = 0.0f;
-	src.m_right = 0.0f;
-	src.m_bottom = 0.0f;
+	src.m_top = 0.0f;   // Y
+	src.m_left = 0.0f;  // X
+	src.m_right = 0.0f; // X
+	src.m_bottom = 0.0f;// Y 
 
-	dst.m_top = 0.0f;
-	dst.m_left = 0.0f;
-	dst.m_right = 0.0f;
-	dst.m_bottom = 0.0;
+	dst.m_top = 0.0f+ my_scroll;
+	dst.m_left = 0.0f+ mx_scroll;
+	dst.m_right = 0.0f+ mx_scroll;
+	dst.m_bottom = 0.0+ my_scroll;
 	Draw::Draw(10, &src, &dst, c, 0.0f);
 
 	//マップチップによるblock設置
