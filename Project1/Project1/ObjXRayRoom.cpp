aae078ee7266c54661a162381ca6757299b5a25b
h1202019034@hiratagakuen.onmicrosoft.com <h1202019034@hiratagakuen.onmicrosoft.com>
2020-11-22 10:00:11 +0900

    修正

--- Project1/Project1/ObjXRayRoom.cpp
+++ Project1/Project1/ObjXRayRoom.cpp
@@ -197,7 +197,7 @@ void CObjXRayRoom::Draw()
 	RECT_F src;//描画元切り取り位置
 	RECT_F dst;//描画先表示位置
 
-	Font::StrDraw(L"病院：レントゲン室", 600, 10, 32, c);
+	Font::StrDraw(L"病院：レントゲン室", 500, 10, 32, c);
 
 	//マップチップによるblock設置
 	for (int i = 0; i < 25; i++)
