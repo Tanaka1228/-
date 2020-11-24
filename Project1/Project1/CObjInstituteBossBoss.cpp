623c31fa421ee69851878c1553897c6410c28aec
泰壱　河村 <h1202019034@hiratagakuen.onmicrosoft.com>
2020-11-24 16:20:28 +0900

    a

--- Project1/Project1/CObjInstituteBossBoss.cpp
+++ Project1/Project1/CObjInstituteBossBoss.cpp
@@ -74,7 +74,7 @@ void CObjInstituteBossBoss::Action()
 
 
 	//ホーミング敵機が完全に領域外に出たら敵機を破棄する
-	bool check = CheckWindow(m_x, m_y, 64.0f, 64.0f, 1600.0f, 900.0f);
+	bool check = CheckWindow(m_x, m_y, -32.0f, -32.0f, 1600.0f, 900.0f);
 	if (check == false)
 	{
 		this->SetStatus(false);//自身に削除命令
