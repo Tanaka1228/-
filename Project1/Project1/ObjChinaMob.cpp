27f2529be9a11801a800d381b81913ac484f209f
ryo12281456@outlook.jp <ryo12281456@outlook.jp>
2020-11-25 16:06:20 +0900

    a

--- Project1/Project1/ObjChinaMob.cpp
+++ Project1/Project1/ObjChinaMob.cpp
@@ -136,6 +136,53 @@ void CObjChinaMob::Action()
 				m_key_control = true;
 			}
 		}
+		if (hero->GetBT() == 9)
+		{
+			if (Input::GetVKey(VK_RETURN) == true) {
+
+				if (m_key_control == true)
+				{
+					if (key_flag == 1)
+					{
+						m_sp = 1;
+
+					}
+
+					if (key_flag == 2)
+					{
+						m_sp = 2;
+
+					}
+					if ((key_flag == 3))
+					{
+
+						m_sp = 3;
+
+					}
+					if ((key_flag == 4))
+					{
+						m_sp = 4;
+
+					}
+					if ((key_flag == 5))
+					{
+						m_sp = 5;
+
+					}
+					if ((key_flag == 6))
+					{
+						m_sp = 6;
+
+					}
+					m_key_control = false;
+				}
+
+			}
+			else
+			{
+				m_key_control = true;
+			}
+		}
 	}
 	
 
@@ -266,6 +313,24 @@ void CObjChinaMob::Draw()
 
 
 			
+			fin.close();//ファイルを閉じる
+		}
+		if (m_sp == 4)//エンターキーを一回押したとき
+		{
+			sp_flag == true;
+
+			ifstream fin("チャイナタウンボス戦フィールドの会話.txt", ios::in);//テキストデータをを読み込み
+			char str1[64];//ただの配列
+			wchar_t wstr1[64];
+			fin.seekg(158, ios::cur);//0バイト数進める
+			fin >> str1;//str1にテキストを入れる
+
+			sprintf_s(str1, "%s", str1);//出力
+			MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str1, 64, wstr1, 64);//文字をユニコードに変換する
+			Font::StrDraw(wstr1, 50.0f, 500, 25, c);// X  Y  大きさ     
+
+
+
 			fin.close();//ファイルを閉じる
 		}
 	}
