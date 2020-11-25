e2f5559c80dc58e224431521bd9adb1ba8395c36
h1202019034@hiratagakuen.onmicrosoft.com <h1202019034@hiratagakuen.onmicrosoft.com>
2020-11-25 21:57:13 +0900

    error

--- Project1/Project1/ObjChinaMob.cpp
+++ Project1/Project1/ObjChinaMob.cpp
@@ -444,109 +444,51 @@ void CObjChinaMob::Draw()
 		}
 	}
 
-	if (chinatownboss != nullptr && mob_flag == 2)
+	
+	
+
+
+
+
+if (chinatown_b != nullptr && mob_flag == 1)
+{
+	if (m_sp == 1)//エンターキーを一回押したとき
 	{
-		if (m_sp == 1)//エンターキーを一回押したとき
-		{
-			sp_flag == true;
+		sp_flag == true;
 
-			ifstream fin("チャイナタウンボス戦フィールドの会話.txt", ios::in);//テキストデータをを読み込み
-			char str1[64];//ただの配列
-			wchar_t wstr1[64];
-			fin.seekg(158, ios::cur);//0バイト数進める
-			fin >> str1;//str1にテキストを入れる
+		ifstream fin("チャイナタウンボス戦フィールドの会話.txt", ios::in);//テキストデータをを読み込み
+		char str1[64];//ただの配列
+		wchar_t wstr1[64];
+		fin.seekg(0, ios::cur);//0バイト数進める
+		fin >> str1;//str1にテキストを入れる
 
-			sprintf_s(str1, "%s", str1);//出力
-			MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str1, 64, wstr1, 64);//文字をユニコードに変換する
-			Font::StrDraw(wstr1, 50.0f, 500, 25, c);// X  Y  大きさ     
+		sprintf_s(str1, "%s", str1);//出力
+		MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str1, 64, wstr1, 64);//文字をユニコードに変換する
+		Font::StrDraw(wstr1, 50.0f, 500, 30, c);// X  Y  大きさ     
 
 
-			key_flag = 2;
-			fin.close();//ファイルを閉じる
-		}
-		if (m_sp == 2)//エンターキーを一回押したとき
-		{
-			sp_flag == true;
-
-			ifstream fin("チャイナタウンボス戦フィールドの会話.txt", ios::in);//テキストデータをを読み込み
-			char str1[64];//ただの配列
-			wchar_t wstr1[64];
-			fin.seekg(158, ios::cur);//0バイト数進める
-			fin >> str1;//str1にテキストを入れる
-
-			sprintf_s(str1, "%s", str1);//出力
-			MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str1, 64, wstr1, 64);//文字をユニコードに変換する
-			Font::StrDraw(wstr1, 50.0f, 500, 25, c);// X  Y  大きさ     
-
-
-			key_flag = 2;
-			fin.close();//ファイルを閉じる
-		}
+		key_flag = 2;
+		fin.close();//ファイルを閉じる
 	}
-
-	if (chinatownboss != nullptr && mob_flag == 2)
+	if (m_sp == 2)//エンターキーを一回押したとき
 	{
-		if (m_sp == 1)//エンターキーを一回押したとき
-		{
-			sp_flag == true;
+		sp_flag == true;
 
-			ifstream fin("チャイナタウンボス戦フィールドの会話.txt", ios::in);//テキストデータをを読み込み
-			char str1[64];//ただの配列
-			wchar_t wstr1[64];
-			fin.seekg(158, ios::cur);//0バイト数進める
-			fin >> str1;//str1にテキストを入れる
+		ifstream fin("チャイナタウンボス戦フィールドの会話.txt", ios::in);//テキストデータをを読み込み
+		char str1[64];//ただの配列
+		wchar_t wstr1[64];
+		fin.seekg(18, ios::cur);//0バイト数進める
+		fin >> str1;//str1にテキストを入れる
 
-			sprintf_s(str1, "%s", str1);//出力
-			MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str1, 64, wstr1, 64);//文字をユニコードに変換する
-			Font::StrDraw(wstr1, 50.0f, 500, 25, c);// X  Y  大きさ     
+		sprintf_s(str1, "%s", str1);//出力
+		MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str1, 64, wstr1, 64);//文字をユニコードに変換する
+		Font::StrDraw(wstr1, 50.0f, 500, 25, c);// X  Y  大きさ     
 
 
-				key_flag = 2;
-				fin.close();//ファイルを閉じる
-			}
-		}
+		key_flag = 3;
+		fin.close();//ファイルを閉じる
 
-	}
-
-
-
-	if (chinatown_b != nullptr && mob_flag == 1)
-	{
-		if (m_sp == 1)//エンターキーを一回押したとき
-		{
-			sp_flag == true;
-
-			ifstream fin("チャイナタウンボス戦フィールドの会話.txt", ios::in);//テキストデータをを読み込み
-			char str1[64];//ただの配列
-			wchar_t wstr1[64];
-			fin.seekg(0, ios::cur);//0バイト数進める
-			fin >> str1;//str1にテキストを入れる
-
-			sprintf_s(str1, "%s", str1);//出力
-			MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str1, 64, wstr1, 64);//文字をユニコードに変換する
-			Font::StrDraw(wstr1, 50.0f, 500, 30, c);// X  Y  大きさ     
-
-
-			key_flag = 2;
-			fin.close();//ファイルを閉じる
-		}
-		if (m_sp == 2)//エンターキーを一回押したとき
-		{
-			sp_flag == true;
-
-			ifstream fin("チャイナタウンボス戦フィールドの会話.txt", ios::in);//テキストデータをを読み込み
-			char str1[64];//ただの配列
-			wchar_t wstr1[64];
-			fin.seekg(18, ios::cur);//0バイト数進める
-			fin >> str1;//str1にテキストを入れる
-
-			sprintf_s(str1, "%s", str1);//出力
-			MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str1, 64, wstr1, 64);//文字をユニコードに変換する
-			Font::StrDraw(wstr1, 50.0f, 500, 25, c);// X  Y  大きさ     
-
-
-			key_flag = 3;
-			fin.close();//ファイルを閉じる
-		}
 	}
 }
+}
+
