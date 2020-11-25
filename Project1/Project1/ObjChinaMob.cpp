4df16870cbf568ebcdc401bf0e4cabf66c4abd5c
DEV1-21\GA1A <h1202016013@hiratagakuen.onmicrosoft.com>
2020-11-25 09:52:14 +0900

    a

--- Project1/Project1/ObjChinaMob.cpp
+++ Project1/Project1/ObjChinaMob.cpp
@@ -34,54 +34,110 @@ void CObjChinaMob::Init()
 void CObjChinaMob::Action()
 {
 	CObjHero* hero = (CObjHero*)Objs::GetObj(OBJ_HERO);
+	CObjChinaTown* chinatown = (CObjChinaTown*)Objs::GetObj(OBJ_CHINA_TOWN);
+	CObjChinaTownBoss* chinatownboss = (CObjChinaTownBoss*)Objs::GetObj(OBJ_CHINA_TOWN_BOSS);
 
-	if (hero->GetBT() ==99)
+	if (chinatown != nullptr)
 	{
-		if (Input::GetVKey(VK_RETURN) == true) {
-
-			if (m_key_control == true)
-			{
-				if (key_flag == 1)
-				{
-					m_sp = 1;
-
-				}
-
-				if (key_flag == 2)
-				{
-					m_sp = 2;
-
-				}
-				if ((key_flag == 3))
-				{
-
-					m_sp = 3;
-
-				}
-				if ((key_flag == 4))
-				{
-					m_sp = 4;
-
-				}
-				if ((key_flag == 5))
-				{
-					m_sp = 5;
-
-				}
-				if ((key_flag == 6))
-				{
-					m_sp = 6;
-
-				}
-				m_key_control = false;
-			}
-
-		}
-		else
+		if (hero->GetBT() == 99)
 		{
-			m_key_control = true;
+			if (Input::GetVKey(VK_RETURN) == true) {
+
+				if (m_key_control == true)
+				{
+					if (key_flag == 1)
+					{
+						m_sp = 1;
+
+					}
+
+					if (key_flag == 2)
+					{
+						m_sp = 2;
+
+					}
+					if ((key_flag == 3))
+					{
+
+						m_sp = 3;
+
+					}
+					if ((key_flag == 4))
+					{
+						m_sp = 4;
+
+					}
+					if ((key_flag == 5))
+					{
+						m_sp = 5;
+
+					}
+					if ((key_flag == 6))
+					{
+						m_sp = 6;
+
+					}
+					m_key_control = false;
+				}
+
+			}
+			else
+			{
+				m_key_control = true;
+			}
 		}
 	}
+	if (chinatownboss != nullptr)
+	{
+		if (hero->GetBT() == 99)
+		{
+			if (Input::GetVKey(VK_RETURN) == true) {
+
+				if (m_key_control == true)
+				{
+					if (key_flag == 1)
+					{
+						m_sp = 1;
+
+					}
+
+					if (key_flag == 2)
+					{
+						m_sp = 2;
+
+					}
+					if ((key_flag == 3))
+					{
+
+						m_sp = 3;
+
+					}
+					if ((key_flag == 4))
+					{
+						m_sp = 4;
+
+					}
+					if ((key_flag == 5))
+					{
+						m_sp = 5;
+
+					}
+					if ((key_flag == 6))
+					{
+						m_sp = 6;
+
+					}
+					m_key_control = false;
+				}
+
+			}
+			else
+			{
+				m_key_control = true;
+			}
+		}
+	}
+	
 
 }
 
@@ -96,8 +152,49 @@ void CObjChinaMob::Draw()
 
 	CObjHero* hero = (CObjHero*)Objs::GetObj(OBJ_HERO);
 	CObjChinaTown* chinatown = (CObjChinaTown*)Objs::GetObj(OBJ_CHINA_TOWN);
-
+	CObjChinaTownBoss* chinatownboss = (CObjChinaTownBoss*)Objs::GetObj(OBJ_CHINA_TOWN_BOSS);
 	if (chinatown != nullptr) 
+	{
+		if (m_sp == 1)//エンターキーを一回押したとき
+		{
+			sp_flag == true;
+
+			ifstream fin("チャイナタウン会話.txt", ios::in);//テキストデータをを読み込み
+			char str1[64];//ただの配列
+			wchar_t wstr1[64];
+			fin.seekg(0, ios::cur);//0バイト数進める
+			fin >> str1;//str1にテキストを入れる
+
+			sprintf_s(str1, "%s", str1);//出力
+			MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str1, 64, wstr1, 64);//文字をユニコードに変換する
+			Font::StrDraw(wstr1, 50.0f, 500, 30, c);// X  Y  大きさ     
+
+
+			key_flag = 2;
+			fin.close();//ファイルを閉じる
+		}
+		if (m_sp == 2)//エンターキーを一回押したとき
+		{
+			sp_flag == true;
+
+			ifstream fin("チャイナタウン会話.txt", ios::in);//テキストデータをを読み込み
+			char str1[64];//ただの配列
+			wchar_t wstr1[64];
+			fin.seekg(46, ios::cur);//0バイト数進める
+			fin >> str1;//str1にテキストを入れる
+
+			sprintf_s(str1, "%s", str1);//出力
+			MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str1, 64, wstr1, 64);//文字をユニコードに変換する
+			Font::StrDraw(wstr1, 50.0f, 500, 25, c);// X  Y  大きさ     
+
+
+			key_flag = 3;
+			fin.close();//ファイルを閉じる
+		}
+	}
+
+	if (chinatownboss != nullptr)
+		CObjChinaTownBoss* chinatownboss = (CObjChinaTownBoss*)Objs::GetObj(OBJ_CHINA_TOWN_BOSS);
 	{
 		if (m_sp == 1)//エンターキーを一回押したとき
 		{
