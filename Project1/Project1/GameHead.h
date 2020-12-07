eb415fe64b1441e7d872a5ba2b4e911aa43d44aa
DEV1-21\GA1A <h1202016013@hiratagakuen.onmicrosoft.com>
2020-12-07 15:58:18 +0900

    シーン切り替え

--- Project1/Project1/GameHead.h
+++ Project1/Project1/GameHead.h
@@ -283,6 +283,7 @@ struct UserData
 //#define SET_GAME_START CSceneGameOver
 #define SET_GAME_START CSceneTitle
 //#define SET_GAME_START CSceneRooftop
+//#define SET_GAME_START CSceneInstitute
 //#define SET_GAME_START CSceneEnd 
 //#define SET_GAME_START CSceneFin
 //#define SET_GAME_START CSceneMain
